aa58d2da2ed4732a29b1a9e2581ae1201ba7af16
Arun Chaudhary <theindiancoder44@gmail.com>
2024-02-26 06:54:14 +0530

    Correct number of items in container to 256 to make 16*16

--- script.js
+++ script.js
@@ -3,16 +3,16 @@
 // Create a container div
 const container = document.createElement("div");
 container.classList.add("container");
-document.body.appendChild(container);
 
-// create a loop to create 16 divs inside container
+// create a loop to create 16*16 divs inside container
+// add all this to html page
 const gridItemList = [];
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
   gridItemList[i] = document.createElement("div");
   gridItemList[i].classList.add("item");
   container.appendChild(gridItemList[i]);
 }
 
-// add all this to html page
+document.body.appendChild(container);
 
 // use css flexbox to arrange the divs in 16 x 16 grid
