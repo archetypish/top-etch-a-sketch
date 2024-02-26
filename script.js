84b0136bc3d510ffb6ccc2268aac389b20d5db18
Arun Chaudhary <theindiancoder44@gmail.com>
2024-02-26 13:03:03 +0530

    Fix code flow to generate static grid using generateGrid function

--- script.js
+++ script.js
@@ -4,32 +4,9 @@
 const container = document.createElement("div");
 container.classList.add("container");
 document.body.appendChild(container);
-// create a loop to create 16*16 divs inside container
-// add all this to html page
-// use css flexbox to arrange the divs in 16 x 16 grid
 
-// setup hover effect when mouse and change color of a div
-
-// register event listener on each item for mouse enter
-const row = [];
-const col = [];
-for (let rowIndex = 0; rowIndex < 16; rowIndex++) {
-  // create a row div which holds individual items in column
-  row[rowIndex] = document.createElement("div");
-  row[rowIndex].classList.add("row");
-  container.appendChild(row[rowIndex]);
-  for (let colIndex = 0; colIndex < 16; colIndex++) {
-    col[colIndex] = document.createElement("div");
-    col[colIndex].classList.add("item");
-
-    row[rowIndex].appendChild(col[colIndex]);
-    col[colIndex].addEventListener("mouseenter", changeBackgroundColor);
-  }
-}
-
-function changeBackgroundColor(e) {
-  e.target.style.backgroundColor = "red";
-}
+// Generate a grid of 16x16 size
+generateGrid(16, 16);
 
 // Add a button to the top of the screen that will send the user
 // a popup asking for the number of squares per side for the new grid.
@@ -44,6 +21,15 @@ btnForUserInput.addEventListener("click", (event) => {
   generateGrid(gridSizeWidth, gridSizeHeight);
 });
 
+// Event Handler to change the background color to red
+
+function changeBackgroundColor(e) {
+  e.target.style.backgroundColor = "red";
+}
+
+// Grid Generator with custom values
+// that support auto grid cell sizing with flex grow
+
 function generateGrid(gridWidth, gridHeight) {
   // remove existing grid
   while (container.firstChild) {
@@ -51,15 +37,19 @@ function generateGrid(gridWidth, gridHeight) {
   }
 
   // create new grid in the same size of the container
-  const gridItemList = [];
-  for (let i = 0; i < gridWidth * gridHeight; i++) {
-    gridItemList[i] = document.createElement("div");
-    gridItemList[i].classList.add(`item`);
-    gridItemList[i].style.width = container.offsetWidth / gridWidth;
-    gridItemList[i].style.height = container.offsetHeight / gridHeight;
-    container.appendChild(gridItemList[i]);
-    gridItemList[i].addEventListener("mouseenter", (event) => {
-      event.target.style.backgroundColor = "red";
-    });
+  const row = [];
+  const col = [];
+  for (let rowIndex = 0; rowIndex < gridHeight; rowIndex++) {
+    // create a row div which holds individual items in column
+    row[rowIndex] = document.createElement("div");
+    row[rowIndex].classList.add("row");
+    container.appendChild(row[rowIndex]);
+    for (let colIndex = 0; colIndex < gridWidth; colIndex++) {
+      col[colIndex] = document.createElement("div");
+      col[colIndex].classList.add("item");
+
+      row[rowIndex].appendChild(col[colIndex]);
+      col[colIndex].addEventListener("mouseenter", changeBackgroundColor);
+    }
   }
 }
