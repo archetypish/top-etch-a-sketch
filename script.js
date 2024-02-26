bcb755a0bde75b8731059cd55aea6cfcb6095cdd
Arun Chaudhary <theindiancoder44@gmail.com>
2024-02-26 07:56:37 +0530

    Change grid item bg color on mouse enter event

--- script.js
+++ script.js
@@ -3,18 +3,21 @@
 // Create a container div
 const container = document.createElement("div");
 container.classList.add("container");
-
+document.body.appendChild(container);
 // create a loop to create 16*16 divs inside container
 // add all this to html page
+// use css flexbox to arrange the divs in 16 x 16 grid
+
+// setup hover effect when mouse and change color of a div
+
+// register event listener on each item for mouse enter
+
 const gridItemList = [];
 for (let i = 0; i < 256; i++) {
   gridItemList[i] = document.createElement("div");
-  gridItemList[i].classList.add("item");
+  gridItemList[i].classList.add(`item`);
   container.appendChild(gridItemList[i]);
+  gridItemList[i].addEventListener("mouseenter", (event) => {
+    event.target.style.backgroundColor = "red";
+  });
 }
-
-document.body.appendChild(container);
-
-// use css flexbox to arrange the divs in 16 x 16 grid
-
-//
