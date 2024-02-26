9049a82f8fde69a5c771a36f06c04f9dc83cfe61
Arun Chaudhary <theindiancoder44@gmail.com>
2024-02-26 12:57:57 +0530

    Update grid cell creation method to prevent grid size hardcoding

--- script.js
+++ script.js
@@ -11,16 +11,26 @@ document.body.appendChild(container);
 // setup hover effect when mouse and change color of a div
 
 // register event listener on each item for mouse enter
+const row = [];
+const col = [];
+for (let rowIndex = 0; rowIndex < 16; rowIndex++) {
+  // create a row div which holds individual items in column
+  row[rowIndex] = document.createElement("div");
+  row[rowIndex].classList.add("row");
+  container.appendChild(row[rowIndex]);
+  for (let colIndex = 0; colIndex < 16; colIndex++) {
+    col[colIndex] = document.createElement("div");
+    col[colIndex].classList.add("item");
 
-const gridItemList = [];
-for (let i = 0; i < 256; i++) {
-  gridItemList[i] = document.createElement("div");
-  gridItemList[i].classList.add(`item`);
-  container.appendChild(gridItemList[i]);
-  gridItemList[i].addEventListener("mouseenter", (event) => {
-    event.target.style.backgroundColor = "red";
-  });
+    row[rowIndex].appendChild(col[colIndex]);
+    col[colIndex].addEventListener("mouseenter", changeBackgroundColor);
+  }
 }
+
+function changeBackgroundColor(e) {
+  e.target.style.backgroundColor = "red";
+}
+
 // Add a button to the top of the screen that will send the user
 // a popup asking for the number of squares per side for the new grid.
 
@@ -34,7 +44,22 @@ btnForUserInput.addEventListener("click", (event) => {
   generateGrid(gridSizeWidth, gridSizeHeight);
 });
 
-function generateGrid(gridSizeWidth, gridSizeHeight) {
+function generateGrid(gridWidth, gridHeight) {
   // remove existing grid
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
   // create new grid in the same size of the container
+  const gridItemList = [];
+  for (let i = 0; i < gridWidth * gridHeight; i++) {
+    gridItemList[i] = document.createElement("div");
+    gridItemList[i].classList.add(`item`);
+    gridItemList[i].style.width = container.offsetWidth / gridWidth;
+    gridItemList[i].style.height = container.offsetHeight / gridHeight;
+    container.appendChild(gridItemList[i]);
+    gridItemList[i].addEventListener("mouseenter", (event) => {
+      event.target.style.backgroundColor = "red";
+    });
+  }
 }
